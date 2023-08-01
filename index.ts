417c68d679b7cbec3a8a446c2b2ef76a9327d3a2
cwfaf <108869786+sweetdream34310@users.noreply.github.com>
2023-08-01 18:57:09 +0000

    server update

--- index.ts
+++ index.ts
@@ -1,9 +1,6 @@
-import { Request, Response, NextFunction } from "express";
 require("dotenv").config(); // Secures variables
 
 const app = require("./utils/app"); // Backend App (server)
-const cosmos = require("./utils/cosmos"); // Cosmos DB
-const cosmos_mongo = require("./utils/cosmos_mongo"); //Cosmos Mongo
 
 const Routes = require("./routes");
 const cors = require("cors");
@@ -13,9 +10,6 @@ app.use("/api", Routes);
 app.use(cors({
     origin: '*'
 }));
-
-cosmos.checkConnection();
-cosmos_mongo.connection();
 
 var http = require("http").createServer(app);
 
